3d7e6963264612a80cfa8cf39622f2ac6ff2bd81
Smit Patel <106525445+Smitpatel0409@users.noreply.github.com>
2024-03-14 21:05:03 +0530

    Add files via upload

---
+++ Sending Mail/app.js
@@ -0,0 +1,100 @@
+const nodemailer =  require('nodemailer');
+const express = require('express')
+const fs = require('fs');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var to;
+var subject;
+var body;
+var path;
+
+var Storage = multer.diskStorage({
+    destination:function(req, file, callback){
+        callback(null, './images');
+    },
+    filename:function(req, file, callback){
+        callback(null, file.fieldname + '_' + Date.now() + '_' +  file.originalname);
+    }
+})
+
+var upload = multer({
+    storage:Storage
+}).single('image');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile('/index.html');
+})
+
+app.post('/sendemail', (req,res) => {
+
+    upload(req, res, function(err){
+        if(err){
+            Console.log(err);
+            return res.end("Something went wrong!");
+        }
+        else{
+            to = req.body.to;
+            subject = req.body.subject;
+            body = req.body.body;
+
+            path = req.file.path;
+
+            console.log(to);
+            console.log(subject);
+            console.log(body);
+            console.log(path);
+            // sendEmail(to, subject, body, path);
+            // res.end("Email has been sent!");
+
+            var transporter = nodemailer.createTransport({
+                service:'gmail' ,
+                auth:{
+                    user:'psmit9077@gmail.com',
+                    pass:'vuxz fffu qxkt ytxh'
+                }
+            })
+
+            var mailOptions = {
+                from:'psmit9077@gmail.com',
+                to: to,
+                subject: subject,
+                text: body,
+                attachments: [
+                    {
+                        path: path,
+                    }
+                ]
+            }
+
+            transporter.sendMail(mailOptions, function(err, info){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log('Email sent:'+ info.response);
+
+                    fs.unlink(path, function(err){
+                        if(err){
+                            return res.end(err);
+                        }else{
+                            console.log('File deleted');
+                            return res.redirect('/result.html');
+                        }
+                    })
+                }
+            })
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
